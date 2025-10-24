2d6351b16a81e5c3eac8e72fa1865c926409df30
Leandro Cavalcante <leandroguitarjesus89@gmail.com>
2025-10-24 15:35:31 -0300

    feat: decrypt

--- util/aes.go
+++ util/aes.go
@@ -5,8 +5,62 @@ import (
 	"crypto/cipher"
 	"crypto/rand"
 	"io"
+	"log"
 	"os"
 )
+
+func DecryptLargeFile(fileInput, fileOutput string, key []byte) error {
+	infile, err := os.Open(fileInput)
+	if err != nil {
+		return err
+	}
+	defer infile.Close()
+	block, err := aes.NewCipher(key) //chave e o principal item de segurança e deve ser passada por varivael de ambiente por exemplo
+	if err != nil {
+		return err
+	}
+	fi, err := infile.Stat() //verifica se arquivo exist, | dar o describe no arquivo
+	if err != nil {
+		return err
+	}
+	iv := make([]byte, block.BlockSize()) // inicialization vector (vertor de inciaalização) e o tamanho e determinado pela key
+	// tamanho da mensagem, vai acontecer um calculo e retirar o iv , por a mensagem original não tem, pois ele foi insetido no final do arquivo
+	msgLen := fi.Size() - int64(len(iv)) // mensagem - o ponto
+	if err != nil {
+		log.Fatal("erro lendo o iv: ", err)
+	}
+
+	// nosso aruivo escriptografado
+	outfile, err := os.OpenFile(fileOutput, os.O_RDWR|os.O_CREATE, 0777)
+	if err != nil {
+		return err
+	}
+	defer outfile.Close()
+	buf := make([]byte, 4096)          //deve ser multiplo de 16bits
+	stream := cipher.NewCTR(block, iv) //como se foce nosso fluxo de dados, lembrar de usar a mesma criptografia (mesmo algoritimo)
+	for {
+		n, err := infile.Read(buf) //o n se refere ao nosso blocos
+		if n > 0 {
+			// verificando se os ultimos bytes são o retorno da inicialização
+			if n > int(msgLen) { //essa validação e para previnir de não tentarmos decriptografar o nosso array de inicialização
+				n = int(msgLen)
+			}
+			msgLen -= int64(n)
+
+			stream.XORKeyStream(buf, buf[:n]) //o xor para reverter(voltar para a configuração original)
+			outfile.Write(buf[:n])            //gravação do buffer já convertido
+		}
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Printf("tamanho de bytes lidos: %d:", n)
+			break
+		}
+	}
+
+	return nil
+}
 
 func EncryptLargeFiles(infile, outfile string, key []byte) error {
 	buf := make([]byte, 4096)  //numero mais adequado ao que faça sentido para vc
