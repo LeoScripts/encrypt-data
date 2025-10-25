4d1e1233765ad7985d3ce3b6a06dac9f15a36e38
Leandro Cavalcante <leandroguitarjesus89@gmail.com>
2025-10-25 00:31:47 -0300

    feat: add decrypt route

--- README.md
+++ README.md
@@ -11,3 +11,10 @@ curl --location --request POST 'http://localhost:7777/encrypt' --form 'file=@"./
 ```bash
 curl --location --request POST 'http://localhost:7777/encrypt' --form 'file=@"./fruit.csv"' > listaDeFrutas.bin
 ```
+
+## decrypt
+
+- with outfile
+```bash
+curl --location --request POST 'http://localhost:7777/decrypt' --form 'file=@"./listaDeFrutas.bin"' > listaDeFrutas-dec.csv
+```
--- main.go
+++ main.go
@@ -14,6 +14,7 @@ import (
 func main() {
 	router := mux.NewRouter()
 	router.HandleFunc("/encrypt", Encrypt).Methods("POST")
+	router.HandleFunc("/decrypt", Decrypt).Methods("POST")
 
 	log.Println("start http encrypt-data server.....")
 	log.Fatal(http.ListenAndServe(":7777", router))
@@ -68,4 +69,58 @@ func Encrypt(w http.ResponseWriter, r *http.Request) {
 	defer os.Remove(tempFileName)
 	defer os.Remove(tempFile.Name())
 
+}
+
+func Decrypt(w http.ResponseWriter, r *http.Request) {
+	//------
+	file, _, err := r.FormFile("file") //no formulario deve ter o campo file
+	if err != nil {
+		log.Printf("erro read file: %v", err)
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	fileName := uuid.New().String()
+	tempFileName := uuid.New().String()
+
+	defer file.Close()
+	fileBytes, err := io.ReadAll(file)
+	if err != nil {
+		log.Printf("erro read file: %v", err)
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	log.Println("read file success")
+	tempFile, err := os.CreateTemp("./", fileName)
+	if err != nil {
+		log.Printf("error create temp file: %v", err)
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	defer tempFile.Close()
+	log.Println("temp file created")
+
+	tempFile.Write(fileBytes)
+	//------
+
+	secretKey := []byte("1234567890asdfgh") // deve vir de uma variavel de ambiente
+	// tempfile.Name = aquivo que vem no upload,
+	// tempFileName =  arquivo criptografado
+	// secretKey = senha de segurança , deve ser igual a do encrypt
+	util.DecryptLargeFile(tempFile.Name(), tempFileName, secretKey)
+	returnFileBytes, err := os.ReadFile(tempFileName) //pegar esse aquivo onde nosso arquivo foi decryptografado
+	if err != nil {
+		log.Printf("error read temp file: %v", err)
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/octet-stream")
+	w.Write(returnFileBytes)
+	defer os.Remove(tempFileName)
+	defer os.Remove(tempFile.Name())
 }
